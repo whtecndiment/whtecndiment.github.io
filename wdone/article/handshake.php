af064f6dbe7138a5d031d5583e633af5f8618b51
whtecndiment <139785762+whtecndiment@users.noreply.github.com>
2023-09-15 21:44:53 +0700

    Delete wdone directory

--- wdone/article/handshake.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Define configuration
-define("DB_HOST", 'localhost');
-define("DB_USER", 'root');
-define("DB_PASS", 'root');
-define("DB_NAME", 'article');
-define("DB_PORT", '3306');
-?>
